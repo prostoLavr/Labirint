8744d17842edc3eaa87d0919b2bb8b32c2bbc45a
prostoLavr <72903956+prostoLavr@users.noreply.github.com>
2021-02-11 20:25:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,25 +20,17 @@ def input_room(text, out_numbers):
 dct_rooms_numbers = {}  # Словарь номеров комнат и объектов.
 
 
-def room_none():
-    pass
-
-
-
-
 class Rooms:
 
-    def __init__(self, number, out_numbers, function=room_none):
+    def __init__(self, number, out_numbers, function=lambda: None):
         self.number = str(number)  # Строка номера комнаты.
         self.out_numbers = out_numbers
         # Словарь напривлений и ссылок на объекты выхода.
         self.function = function
-        print(self.function)
         global dct_rooms_numbers
         dct_rooms_numbers.update({self.number: self})
 
     def start(self):
-        # print_room(Data.text_room[self.number])
         self.function()  # Вызываем функцию текущей комнаты, если она есть.
         input_room(Data.text_room[self.number], self.out_numbers)  # Ввод и переход к следующей комнате.
 
@@ -49,14 +41,9 @@ def room_4():  # Функция 4-й комнаты.
     print('Ok!')
 
 
-def get_flag(room_number):
-    pass
-
-
 room1 = Rooms(1, {'n': 2})
 room2 = Rooms(2, {'s': 1, 'n': 3, 'w': 4})
-room3 = Rooms(3, {'s': 2, 'cry': get_flag(3)})
+room3 = Rooms(3, {'s': 2})
 room4 = Rooms(4, {'e': 2, 'n': 5}, room_4)
 room5 = Rooms(5, {'s': 4})
-print(dct_rooms_numbers)
 room1.start()
