c1588bc4f38b79a78eecd12ac7bb94b82db4b74a
prostoLavr <72903956+prostoLavr@users.noreply.github.com>
2021-02-15 08:10:17 +0800

    преапдейт ввода
    Signed-off-by: prostoLavr <72903956+prostoLavr@users.noreply.github.com>

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ def print_room(text):
 
 def input_room(text):
     print(f'{Data.next_room_text} ({", ".join(text)})')
-    return input().lower()
+    text_input = input().lower()
+    while text_input and text_input not in text:
+        print_room('NameError')
+        text_input = input().lower()
+    return text_input
 
 
 def next_room(out_numbers, flags):
 
     # Вывод текста и вариантов ответа.
 
-    text_input = input().lower()
-    while text_input and text_input not in list(out_numbers.keys()) + list(flags.keys()):
-        print_room('NameError')
-        text_input = input().lower()
+    text_input = input_room(list(out_numbers.keys()) + list(flags.keys()))
 
     if text_input:
 
@@ -77,9 +78,9 @@ def room_5():
     return False  # Не вызывать стандартный ввод
 
 
-room1 = Rooms(1, {'n': 2}, {'cry': 'cry_flag_1'})
-Rooms(2, {'s': 1, 'n': 3, 'w': 4})
+Rooms(1, {'n': 2}, {'cry': 'cry_flag_1'})
+Rooms(2, {'s': 1, 'n': 3, 'w': 4}, {'cry': 'cry_flag_2'})
 Rooms(3, {'s': 2})
 Rooms(4, {'e': 2, 'n': 5}, function=room_4)
 Rooms(5, {'s': 4}, function=room_5)
-room1.start()
+dct_rooms_numbers['1'].start()
