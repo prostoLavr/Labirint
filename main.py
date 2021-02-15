8642807525aca346379d6f7f84418a3ae927cab5
prostoLavr <72903956+prostoLavr@users.noreply.github.com>
2021-02-15 09:00:04 +0800

    Изменение структуры
    Signed-off-by: prostoLavr <72903956+prostoLavr@users.noreply.github.com>

--- main.py
+++ main.py
@@ -14,73 +14,47 @@ def input_room(text):
     return text_input
 
 
-def next_room(out_numbers, flags):
-
-    # Вывод текста и вариантов ответа.
-
-    text_input = input_room(list(out_numbers.keys()) + list(flags.keys()))
-
-    if text_input:
-
-        if text_input in out_numbers.keys():
-            dct_rooms_numbers[str(out_numbers[text_input])].start()  # Вызов комнаты из словаря по изначению комнаты.
-
-        else:
-            if not dct_flags[flags[text_input]]:
-                dct_flags[flags[text_input]] = True
-                print_room(Data.flags[flags[text_input]])
-            else:
-                print_room('Если тут что-то и было, это забрали')
-            next_room(out_numbers, flags)
-
-    else:
-        print('Bye!')
-
-
-dct_rooms_numbers = {}  # Словарь {номер комнаты: объект класса Rooms}.
 dct_flags = {}
 
 
 class Rooms:
 
-    def __init__(self, number, out_numbers, flags=None, function=lambda: True):
-        if flags is None:
-            flags = {}
-
+    def __init__(self, number, text, out_numbers, flags=None, function=lambda: True):
         if flags:
             for i in flags.values():
                 if i not in dct_flags.keys():
                     dct_flags.update({i: False})
-
+        self.text = text
         self.number = str(number)  # Строка номера комнаты.
         self.out_numbers = out_numbers
         self.flags = flags
         # Словарь {направление: ссылка на объект}.
         self.function = function
-        global dct_rooms_numbers
-        dct_rooms_numbers.update({self.number: self})
 
-    def start(self):
-        print_room(Data.text_room[self.number])
+    def start(self, dct):
+        print_room(self.text)
         if self.function():  # Вызываем функцию текущей комнаты, если не была передана, вызываем пустую.
-            next_room(self.out_numbers, self.flags)  # Ввод и переход к следующей комнате.
+            self.do_room(dct)  # Ввод и переход к следующей комнате.
+
+    def do_room(self, dct):
+        # Вывод текста и вариантов ответа.
+        text_input = input_room(list(self.out_numbers.keys()) + list(self.flags.keys()))
+        if text_input:
+            if text_input in self.out_numbers.keys():
+                dct[int(self.out_numbers[text_input])].start(dct)
+                # Вызов комнаты из словаря по изначению комнаты.
+            if text_input in self.flags.keys():
+                if not dct_flags[self.flags[text_input]]:
+                    dct_flags[self.flags[text_input]] = True
+                    print_room(Data.flag_texts[self.flags[text_input]])
+                else:
+                    print_room('Если тут что-то и было, это забрали')
+                self.do_room(dct)
+        else:
+            print('Bye!')
 
 
-def room_4():  # Функция 4-й комнаты.
-    print('Hello! How are U?')
-    input()
-    print('Ok!')
-    return True  # Продолжить стандартным вводом
-
-
-def room_5():
-    print("It's the end.")
-    return False  # Не вызывать стандартный ввод
-
-
-Rooms(1, {'n': 2}, {'cry': 'cry_flag_1'})
-Rooms(2, {'s': 1, 'n': 3, 'w': 4}, {'cry': 'cry_flag_2'})
-Rooms(3, {'s': 2})
-Rooms(4, {'e': 2, 'n': 5}, function=room_4)
-Rooms(5, {'s': 4}, function=room_5)
-dct_rooms_numbers['1'].start()
+dct_rooms = {}
+for element in Data.dct_main:
+    dct_rooms.update({element[0]: Rooms(*element)})
+dct_rooms[1].start(dct_rooms)
